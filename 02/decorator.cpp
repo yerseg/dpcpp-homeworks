86bfd3bd1fb1f11e6d6f2864ef3887332ee91385
Аня Кульмаметова <95079228+akulm@users.noreply.github.com>
2023-02-14 17:45:01 +0300

    Create decorator.cpp

---
+++ 02/decorator.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <iostream>
+
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+};
+
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text_(text) {}
+    Text* text_;
+};
+
+class ItalicText : public DecoratedText {
+public:
+    explicit ItalicText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data)  const  {
+        std::cout << "<i>";
+        text_->render(data);
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText {
+public:
+    explicit BoldText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
+
+int main() {
+    auto text_block = new ItalicText(new BoldText(new Text()));
+    text_block->render("Hello world");
+}
